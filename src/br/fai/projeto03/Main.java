8e8e60870536dae14e53bdf19e43f3a1f30726df
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:12:26 -0300

    alteracao

--- src/br/fai/projeto03/Main.java
+++ src/br/fai/projeto03/Main.java
@@ -1,10 +1,33 @@
 package br.fai.projeto03;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Main app = new Main();
+		app.start();
 
+	}
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	private void start() {
+		
+		System.out.println("Digite uma idade: ");
+		
+		int idade = scanner.nextInt();
+		
+		if(idade > 20) {
+			System.out.println("idade maior que 20");
+		}else if(idade > 12 && idade < 17) {
+			System.out.println("Idade maior que 12 e menor que 17");
+		}else if(idade == 20 || idade < 5) {
+			System.out.println("Idade igual a 7 OU menor que 5");
+		}else {
+			System.out.println("Outras combinações");
+		}
 	}
 
 }
